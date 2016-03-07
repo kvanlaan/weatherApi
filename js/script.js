2927047c8276035391bdbe04c3b87730b27f5e16
Katrina Van Laan <kdvanlaan@gmail.com>
2016-03-07 01:06:55 -0600

    update

--- js/script.js
+++ js/script.js
@@ -3,25 +3,19 @@ console.log($)
 
 ///
 
-///time
-
-
-
-
-
 //
 ///global variables
-var apiKey = "08436633d268865567452ff70c3673ea"
-var baseUrl = "https://api.forecast.io/forecast/"
+var apiKey = "08436633d268865567452ff70c3673ea/"
+var baseUrl = "https://api.forecast.io/forecast/" + apiKey
 
 var lat = "29.7604"
 var lon = "95.3698"
-var fullUrl = baseUrl + apiKey + "/" + lat + "," + lon
+var fullUrl = baseUrl + lat + "," + lon
 var date = new Date()
-
-var containerEl = document.querySelector("#container"), 
+var inputEl = document.querySelector("input"),
+	containerEl = document.querySelector("#container"), 
 	currently = document.querySelector('#buttons button[value="currently"]'), 
-	daily = document.querySelector('#buttons button[value="daily"]')
+	daily = document.querySelector('#buttons button[value="daily"]'),
 	hourly = document.querySelector('#buttons button[value="hourly"]')
 //container attachment to HTML sheet 
 // ----------- Functions -------------- //
@@ -37,8 +31,14 @@ var currentHtml = function(jsonData) {  //We want to iterate over the array data
 }
 
 ///daily
+var changeView = function(event) { // this function should change the hash depending on what button was clicked. we'll let the controller take it from there. 
+	var buttonEl = event.target, // target = the node wherein the event occured
+		currentQuery = location.hash.split('/')[1] // before we rewrite the hash, we want to make sure we preserve the query value from the pre-existing hash
+		// Location.hash stores the text that comes after the # in the url. The split will separate the view type from the search term, storing the two of them in a short array. We want to locate the actual query word, which will be at index 1 in the array. Then this will be assigned to the variable currentQuery.
+	location.hash = buttonEl.value + '/' + currentQuery // What we're changing is the view type, which was stored in the button's value attribute (check the html to verify this). After that, we tack on the old currentQuery that we read from the hash. 
+}
 
-
+///
 var dailyHtml = function(jsonData) {  //We want to iterate over the array data, using it to build an htmlString that we will output.
 	// Initializes the empty string of HTML that we will build. 
 	htmlString = ""
@@ -51,10 +51,14 @@ var dailyHtml = function(jsonData) {  //We want to iterate over the array data, 
 }
 return htmlString
 }
+///do search request
+
+
+	 
 
 //hourly
 
-
+///
 var hourlyHtml = function(jsonData) {  //We want to iterate over the array data, using it to build an htmlString that we will output.
 	// Initializes the empty string of HTML that we will build. 
 	htmlString = ""
@@ -79,13 +83,21 @@ window.onload = function() {
 
   fullUrl 
 }
-////
-var changeView = function(event) { // this function should change the hash depending on what button was clicked. we'll let the controller take it from there. 
-	var buttonEl = event.target, // target = the node wherein the event occured
-		currentQuery = location.hash.split('/')[1] // before we rewrite the hash, we want to make sure we preserve the query value from the pre-existing hash
-		// Location.hash stores the text that comes after the # in the url. The split will separate the view type from the search term, storing the two of them in a short array. We want to locate the actual query word, which will be at index 1 in the array. Then this will be assigned to the variable currentQuery.
-	location.hash = buttonEl.value + '/' + currentQuery // What we're changing is the view type, which was stored in the button's value attribute (check the html to verify this). After that, we tack on the old currentQuery that we read from the hash. 
+///////
+///activate the search funciton
+var handleUserInput = function(keyEvent) {
+	var inputEl = document.querySelector("input")
+		
+	if (keyEvent.keyCode === 13) {
+		var inputEl = keyEvent.target
+		var query = inputEl.value
+		location.hash = query
+		inputEl.value = ""
+	fullUrl = baseUrl + query 
 }
+return fullUrl
+}
+///
 var hashController = function(){ // Will read the hash and invoke the proper function
 	var route = window.location.hash.substring(1) //route stores whatever comes after the # (but does not include the '#', because we substringed it out)
 	var routeParts = route.split('/'), // We want to split "<view type>/<search word>" into an array, so that we can separate the view type from the search term and store them in separate variables. 
@@ -103,7 +115,9 @@ var hashController = function(){ // Will read the hash and invoke the proper fun
 		renderHourlyView(currentQuery)
 		// this is the only one that actually does anything atm
 	} 
+
 } 	
+///
 var renderCurrentView = function(query) { //This function will run when the condition in our hashController function is met (so, when viewType === "scroll")
 	
 	var paramObj = { //This is what's' being passed through our url, and the url is being passed through our promise. The json library will do its thang. I.e. this object will be parsed into a parameter string of the form "?key=value&key2=value2..." and concatenated onto our baseUrl.
@@ -159,9 +173,9 @@ daily.addEventListener("click",changeView)
 
 hourly.addEventListener("click",changeView)
 // ----------- Kick things off! -------------- //
+inputEl.addEventListener("keydown",handleUserInput)
+//
 hashController() 
 // If the page is being loaded for the first time, there is no hash*change* event! so, 
 // in this example, we manually invoke the hashController on the first page load. this is not always
 // the way things will happen.
-
-
