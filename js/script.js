dd46f0aaf6900ef30832def6a703d7732e64e30b
Katrina Van Laan <kdvanlaan@gmail.com>
2016-03-07 17:18:37 -0600

    update

--- js/script.js
+++ js/script.js
@@ -1,157 +1,143 @@
 console.log("hello")
 console.log($)
-    ///global variables
-var apiKey = "08436633d268865567452ff70c3673ea/"
-var baseUrl = "https://api.forecast.io/forecast/" + apiKey
 
-var lat = "29.7604"
-var lon = "95.3698"
-var fullUrl = baseUrl + lat + "," + lon
+//date
 var date = new Date()
+
+//queryselectors
 var inputEl = document.querySelector("input"),
     containerEl = document.querySelector("#container"),
     currently = document.querySelector('#buttons button[value="currently"]'),
     daily = document.querySelector('#buttons button[value="daily"]'),
     hourly = document.querySelector('#buttons button[value="hourly"]')
-    //container attachment to HTML sheet 
-    // ----------- Functions -------------- //
+
+// Functions in alphabetical order//
+
+///changeview, making sure the buttons work
+var changeView = function(clickEvent) {
+	var route = window.location.hash.substr(1),
+		routeParts = route.split('/'),
+		lat = routeParts[1],
+		lon = routeParts[2]
+
+	var buttonEl = clickEvent.target,
+		newView = buttonEl.value
+	location.hash = newView + "/" + lat + "/" + lon
+}
+///making the different htmlstrings///
 var currentHtml = function(jsonData) { 
     var htmlString = ""
-    htmlString += "Current Weather"
-    htmlString += "<p>" + jsonData.currently.apparentTemperature + "C" + "</p>"
-    htmlString += "<p>" + jsonData.currently.summary + "</p>"
-    htmlString += "<p>" + date + "</p>"
-    return htmlString
+    htmlString += "<h1> Current Weather </h1>"
+    htmlString += "<p><h2> " + jsonData.currently.apparentTemperature + " &deg;C " + " </h2></p>"
+    htmlString += "<p><h3> " + jsonData.currently.summary + " </h3></p>"
+    htmlString += "<p> " + date + " </p>"
     console.log(htmlString)
+    containerEl.innerHTML = htmlString
 }
-
-///changview
-var changeView = function(event) {
-    var buttonEl = event.target, 
-        currentQuery = location.hash.split('/')[1] 
-    location.hash = buttonEl.value + '/' + currentQuery 
-}
-
 ///daily
 var dailyHtml = function(jsonData) { 
-        htmlString = ""
+        htmlString = "<h1> 7-Day Forecast </h1>"
         var newArr = jsonData.daily.data
-        for (var i = 0; i < 5; i++) { 
+        for (var i = 0; i < 7; i++) { 
             var obj = newArr[i]
-            htmlString += "Day " + i + " Weather"
-            htmlString += "<p>" + obj.apparentTemperatureMax + "C" + "</p>"
+            htmlString += "<h2> Day " + i + " Weather </h2>"
+            htmlString += "<p><h2>" + obj.apparentTemperatureMax + " &deg;C" + "</h2></p>"
 
         }
-        return htmlString
+        containerEl.innerHTML = htmlString
     }
-//hourly
-var hourlyHtml = function(jsonData) { //We want to iterate over the array data, using it to build an htmlString that we will output.
-        // Initializes the empty string of HTML that we will build. 
-        htmlString = ""
-        var newArr = jsonData.hourly.data
-        for (var i = 0; i < 5; i++) { //We'll only use the first five images.
-            var obj = newArr[i]
-            htmlString += "Hour " + i + " Weather"
-            htmlString += "<p>" + obj.apparentTemperature + "C" + "</p>"
-        }
-        htmlString += "<p>" + date + "</p>"
-        return htmlString
-    }
-    // geolocation
-window.onload = function() {
-        var startPos
-        var geoSuccess = function(position) {
-            startPos = position
-            lat = startPos.coords.latitude
-            lon = startPos.coords.longitude
+
+///geolocation
+var handleDefault = function() {
+        var geoSuccess = function(positionObject) {
+        var lat = positionObject.coords.latitude
+        var lon = positionObject.coords.longitude
+	location.hash = "currently/" + lat + "/" + lon
+
         }
         navigator.geolocation.getCurrentPosition(geoSuccess)
-
-        fullUrl
-    }
-    ///activate the search funciton
+}
+///activate the search funciton
 var handleUserInput = function(keyEvent) {
         var inputEl = document.querySelector("input")
-
         if (keyEvent.keyCode === 13) {
             var inputEl = keyEvent.target
             var query = inputEl.value
             location.hash = query
             inputEl.value = ""
-            fullUrl = baseUrl + query
+           
         }
-        return fullUrl
+     var searchPromise = $.getJSON(fullUrl)
+     searchPromise.then(handleData)
     }
-    ///
+///
 var hashController = function() { 
         var route = window.location.hash.substring(1) 
         var routeParts = route.split('/'),
             viewType = routeParts[0], 
-            currentQuery = routeParts[1] 
+            lat = routeParts[1], 
+            lon = routeParts[2]
+        if (viewType === "") {
+        	handleDefault() 	
+        }
         if (viewType === "currently") {
-            renderCurrentView(currentQuery)
-           
+            renderCurrentView(lat, lon)
         }
         if (viewType === "daily") {
-            renderDailyView(currentQuery)
-
+            renderDailyView(lat, lon)
         }
         if (viewType === "hourly") {
-            renderHourlyView(currentQuery)
+            renderHourlyView(lat,lon)
         }
-
     }
-    ///
-var renderCurrentView = function(query) { 
-
-        var paramObj = { 
-            q: query,
-            api_key: apiKey
+//hourlyhtml
+var hourlyHtml = function(jsonData) { //We want to iterate over the array data, using it to build an htmlString that we will output.
+        // Initializes the empty string of HTML that we will build. 
+        htmlString = "<h1> Hourly Forecast </h1>"
+        var newArr = jsonData.hourly.data
+        for (var i = 0; i < 5; i++) { //We'll only use the first five images.
+            var obj = newArr[i]
+            htmlString += " Hour " + i + " Weather "
+            htmlString += "<p>" + obj.apparentTemperature + " &deg;C " + "</p>"
         }
-        var weatherPromise = $.getJSON(fullUrl, paramObj)
-        var handleData = function(jsonData) { 
-            console.log(jsonData)
-            containerEl.innerHTML = currentHtml(jsonData) 
-        }
-        weatherPromise.then(handleData) //Once the data has been retrieved, our promise will run our handleData function
+        htmlString += "<p> " + date + " </p>"
+        containerEl.innerHTML = htmlString
     }
-    // ----------- Event Listeners -------------- //
 
-var renderDailyView = function(query) { 
-
-        var paramObj = { 
-            q: query,
-            api_key: apiKey
-        }
-        var weatherPromise = $.getJSON(fullUrl, paramObj) 
-        var handleData = function(jsonData) { 
-            console.log(jsonData)
-            containerEl.innerHTML = dailyHtml(jsonData) 
-        }
-        weatherPromise.then(handleData) 
+///promisemaker
+var makePromise = function(lat,lon) { 
+		var fullUrl = baseUrl + lat + "," + lon 
+        var promise = $.getJSON(fullUrl)
+     	return promise 
     }
-    ///
-
-
-var renderHourlyView = function(query) { 
-        var paramObj = { 
-            q: query,
-            api_key: apiKey
-        }
-        var weatherPromise = $.getJSON(fullUrl, paramObj) 
-        var handleData = function(jsonData) { 
-            console.log(jsonData)
-            containerEl.innerHTML = hourlyHtml(jsonData)
-        }
-        weatherPromise.then(handleData) 
+///rendering(gettingthepromise)
+//current
+var renderCurrentView = function(lat,lon) { 
+      var promise = makePromise(lat,lon)
+      promise.then(currentHtml)
     }
-    // ----------- Event Listeners -------------- //
+
+var renderDailyView = function(lat,lon) { 
+     var promise = makePromise(lat,lon)
+      promise.then(dailyHtml)
+    }
+///
+var renderHourlyView = function(lat,lon) { 
+      var promise = makePromise(lat,lon)
+      promise.then(hourlyHtml)
+    }
+// ----------- Event Listeners -------------- //
 window.addEventListener("hashchange", hashController)
 currently.addEventListener("click", changeView)
 daily.addEventListener("click", changeView)
-
 hourly.addEventListener("click", changeView)
-    // ----------- Kick things off! -------------- //
 inputEl.addEventListener("keydown", handleUserInput)
-hashController()
 
+
+//
+var apiKey = "95dd0186251e48c64682d50d1e64004c/"
+var baseUrl = "https://api.forecast.io/forecast/" + apiKey
+
+//
+// ----------- Kick things off! -------------- //
+hashController()
